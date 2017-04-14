150b73bb6e128ac42a8e4e0e8f0cf2e258f51312
Vikas Solegaonkar <vikas.solegaonkar@gmail.com>
2017-04-14 17:31:33 +0530

    Added code from vinigodoy. Import it in pieces and then discard rest

---
+++ src/br/com/vinigodoy/image/transform/Negation.java
@@ -0,0 +1,28 @@
+/*===========================================================================
+COPYRIGHT 2013 Vinícius G. Mendonça ALL RIGHTS RESERVED.
+
+This software cannot be copied, stored, distributed without
+Vinícius G. Mendonça prior authorization.
+
+This file was made available on https://github.com/ViniGodoy and it
+is free to be redistributed or used under Creative Commons license 2.5 br:
+http://creativecommons.org/licenses/by-sa/2.5/br/
+============================================================================*/
+package br.com.vinigodoy.image.transform;
+
+import br.com.vinigodoy.image.color.ColorChannel;
+import br.com.vinigodoy.image.filters.PixelByPixelFilter;
+
+import java.awt.image.BufferedImage;
+
+public class Negation extends PixelByPixelFilter {
+    public Negation(ColorChannel... channels) {
+        super(channels);
+    }
+
+    @Override
+    protected int processChannel(BufferedImage img, ColorChannel channel, int x,
+                                 int y, int tone) {
+        return channel.tones() - tone - 1;
+    }
+}
