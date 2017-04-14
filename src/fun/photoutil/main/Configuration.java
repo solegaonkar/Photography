dcee0adf6f9e581dc4e3002488cac10b711c4465
Vikas Solegaonkar <vikas.solegaonkar@gmail.com>
2017-04-14 18:04:39 +0530

    EOD - 2017 04 14

--- src/fun/photoutil/main/Configuration.java
+++ src/fun/photoutil/main/Configuration.java
@@ -1,6 +1,7 @@
 package fun.photoutil.main;
 
 import java.io.FileReader;
+import java.io.FileWriter;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -21,6 +22,28 @@ public class Configuration {
 			if (jr != null)
 				try {
 					jr.close();
+				} catch (Exception e) {
+				}
+		}
+	}
+
+	public String get(String key) {
+		return (String) json.get(key);
+	}
+
+	@SuppressWarnings("unchecked")
+	public void set(String key, String value) {
+		json.put(key, value);
+		FileWriter jw = null;
+		try {
+			jw = new FileWriter(Constants.CONFIGURATION_FILE);
+			json.writeJSONString(jw);
+		} catch (Exception e) {
+			CommonUtils.exception(e);
+		} finally {
+			if (jw != null)
+				try {
+					jw.close();
 				} catch (Exception e) {
 				}
 		}
